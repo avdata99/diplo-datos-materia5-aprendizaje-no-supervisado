98434af64989843ae82d26cb9d14f69d4893e0ab
Andrés Vázquez <andres@data99.com.ar>
2019-08-24 21:48:14 -0300

    script > notebook for CCAD

--- practico/movies.py
+++ practico/movies.py
@@ -1,15 +1,20 @@
-
-# coding: utf-8
-
-# In[1]:
-
-
 import pandas as pd
 import numpy as np
 import sys
 from itertools import groupby
-from IPython.display import display
 import os
+import csv
+from efficient_apriori import apriori
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("--min_support", type=float, help="Min support", required=True)
+parser.add_argument("--max_length", type=int, default=2, help="Max length")
+parser.add_argument("--min_confidence", type=float, help="Min confidence", required=True)
+parser.add_argument("--min_lift", type=int, default=4, help="Min lift to count")
+parser.add_argument("--truncate_transactions", type=int, default=4, help="truncate total transactions (for local test)")
+
+args = parser.parse_args()
 
 # Descripcion de los datos:
 # head movies.csv:
@@ -41,111 +46,116 @@ import os
 """
 
 data_folder = '/media/hudson/Elements/data/DiploDatos-aprendizaje-no-supervizado/ml-20m'
+# data_folder = 'ml-20m'
 movies_path = os.path.join(data_folder, 'movies.csv')
 ratings_path = os.path.join(data_folder, 'ratings.csv')
 
 def size(obj):
     return "{0:.2f} MB".format(sys.getsizeof(obj) / (1000 * 1000))
 
-
-# In[2]:
-
-
 movies = pd.read_csv(movies_path)
 print('movies -- dimensions: {0};   size: {1}'.format(movies.shape, size(movies)))
-display(movies.head())
+print(movies.head())
 
-
-# In[3]:
-
-
-import matplotlib.pyplot as plt
-movies['splitted_genres'] = movies['genres'].str.split('|', n=0, expand=False)
-display(movies.head())
-# generos = movies['splitted_genres'].unique()
-count_by_genre = movies['splitted_genres'].apply(pd.Series).stack().value_counts()
-display(count_by_genre)
-count_by_genre.plot.bar(figsize=(16, 4))
-
-
-# In[4]:
-
-
-# Obtener los dummies sobre un campo que tiene listas dentro
-# https://stackoverflow.com/questions/47026585/unhashable-type-list-error-with-get-dumies
-# pd.get_dummies(df.categories.apply(pd.Series).stack()).sum(level=0)
-
-dummy_genres = pd.get_dummies(movies['splitted_genres'].apply(pd.Series).stack()).sum(level=0)
-movies_with_dummies_genres = pd.concat([movies, dummy_genres], axis=1)
-# se quitan las dos ultimas categorías que tiene pocas películas
-movies_cleaned = movies_with_dummies_genres.drop(columns=['genres', 'splitted_genres', 'IMAX','(no genres listed)'])
-display(movies_cleaned.head())
-
-
-# In[5]:
-
+movies_cleaned = movies.drop(columns=['genres'])
+print(movies_cleaned.head())
 
 ratings = pd.read_csv(ratings_path, usecols = ['userId', 'movieId', 'rating'])
 print('ratings -- dimensions: {0};   size: {1}'.format(ratings.shape, size(ratings)))
-display(ratings.head())
-
-
-# In[6]:
-
+print(ratings.head())
 
 # Cuantos ratings hay con cada puntuacion?
 # No me interesan los bajos. Entiendo que "comprar" el item es darle un buen rating
 count_by_rank = ratings.groupby('rating').size()
-display(count_by_rank)
-count_by_rank.plot.bar(figsize=(16, 4))
-
-
-# In[7]:
-
+print(count_by_rank)
 
 # voy a tomar solo los ratings > a lo que defino
 min_rating_to_count = 5
 ratings_good = ratings[ratings['rating'] >= min_rating_to_count]
 print('ratings_good -- dimensions: {0};   size: {1}'.format(ratings_good.shape, size(ratings_good)))
-display(ratings_good.head())
-
-
-# In[8]:
-
+print(ratings_good.head())
 
 # tomo al "userid" como el identificador de transaccion.
 # Interpreto que lo cada uusario vio y valoro positivamente es un "compra"
 # Quitar el rating que no es necesario
-ratings_cleaned = ratings.drop(columns=['rating'])
-
-# identificar las transacciones (IDUSUARIO = [ID_PELI_1, ID_PELI_n, ID_PELI_m, ...])
-transactions = ratings_cleaned.groupby('userId')['movieId'].apply(list)
-print('------------- Transacciones (solo algunas) -----------------')
-print(transactions[:20])
-
-
-# In[ ]:
-
-
-from efficient_apriori import apriori
+ratings_cleaned = ratings.drop(columns=['rating']).sort_values(by=['userId', 'movieId'])
+transactions = ratings_cleaned.values.tolist()
+if args.truncate_transactions > 0:
+    print('******\n******\nTRUNCATE TRANSACTIONS TO {}******\n******\n'.format(args.truncate_transactions))
+    transactions = transactions[:args.truncate_transactions]
+print('First transaction: {}'.format(transactions[0]))
 
 print('Transacciones encontradas: {}'.format(len(transactions)))
-# para probar solo con algunos
-transactions = transactions[:10]
 
-min_support = 0.1
-min_confidence = 0.8
-min_lift = 4
-itemsets, rules = apriori(transactions, min_support=min_support,  min_confidence=min_confidence)
+def gen_transactions():
+    for t in transactions:
+        yield t
 
-print(f'------------- RULES Support:{min_support} Confidence:{min_confidence}-----------------')
+itemsets, rules = apriori(gen_transactions(),
+                          max_length=args.max_length,
+                          min_support=args.min_support,
+                          min_confidence=args.min_confidence,
+                          # verbosity=2
+                          )
+
+print(f'------------- RULES Support:{args.min_support} Confidence:{args.min_confidence}-----------------')
 #rules = filter(lambda rule: len(rule.lhs) == 2 and len(rule.rhs) == 1, rules)
-rules=sorted(rules, key=lambda rule: rule.confidence)
+print('Ordenando ..')
+rules = sorted(rules, key=lambda rule: rule.lift, reverse=True)
+
+def get_movie_title(movieId):
+    flt = movies['movieId'] == movieId
+    rows = movies[flt]
+    return list(rows['title'])[0]
+
 selected_rules = []
 for rule in rules:
-    if rule.lift > min_lift:
-        selected_rules.append(rule)
-        print(rule) # Prints the rule and its confidence, support, lift, ...
+    if rule.lift > args.min_lift:
+        jrule = {
+            'izq': rule.lhs[0],
+            'izq_title': get_movie_title(rule.lhs[0]),
+            'der': rule.rhs[0],
+            'der_title': get_movie_title(rule.rhs[0]),
+            'conf': round(rule.confidence, 2),
+            'supp': round(rule.support, 2),
+            'lift': round(rule.lift, 2),
+            'conv': round(rule.conviction, 2)
+        }
+        selected_rules.append(jrule)
+        if len(selected_rules) < 20:
+            print(rule) # Prints the rule and its confidence, support, lift, ...
 
-print('FIN')
+print('Reglas encontradas: {}'.format(len(selected_rules)))
 
+f = open('rules.csv', 'w')
+fieldnames = selected_rules[0].keys()
+writer = csv.DictWriter(f, fieldnames=fieldnames)
+writer.writeheader()
+for jrule in selected_rules:
+    writer.writerow(jrule)
+f.close()
+
+"""
+import json
+jrules_str = json.dumps(selected_rules, indent=2)
+f = open('jrules.json', 'w')
+f.write(jrules_str)
+f.close()
+"""
+
+# ver que peliculas son 
+f = open('summary.txt', 'w')
+for jrule in selected_rules:
+    
+    txt = ('Con una coinfianza de {} (y lift {}) '
+            'a los que le gusto la película "{}" '
+            'les gusto tambien "{}"\n'.format(jrule['conf'],
+                                          jrule['lift'],
+                                          jrule['izq_title'],
+                                          jrule['der_title'],
+                                          ))
+    f.write(txt)
+    if len(selected_rules) < 20:
+        print(txt)
+
+f.close()
